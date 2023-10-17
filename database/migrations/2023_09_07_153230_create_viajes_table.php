01b3426823dc8bd4a6ebcffe3c59f7e5d610bb4e
Luis Salazar <41168258+nightstormy@users.noreply.github.com>
2023-10-17 13:05:33 -0400

    Update 2023_09_07_153230_create_viajes_table.php

--- database/migrations/2023_09_07_153230_create_viajes_table.php
+++ database/migrations/2023_09_07_153230_create_viajes_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -16,7 +15,7 @@ return new class extends Migration
         Schema::create('viajes', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('transport_id');
+            $table->unsignedBigInteger('transport_id')->nullable();
             $table->string('ruta')->nullable();
             $table->string('tiempo')->nullable();
             $table->time('hora')->nullable();
